5f98500f3c0224ba39ecad208ce4565ee561c329
jonxmitchell <jonxmitchell@outlook.com>
2023-01-03 18:29:38 +0000

    added variables and formatting to console logs

--- index.js
+++ index.js
@@ -21,10 +21,12 @@ Steam.ready(async function (err) {
     modIDs.forEach((modID) => {
       steam.getPublishedFileDetails({ itemcount: "1", "publishedfileids[0]": `${modID}` }, async function (err, data) {
         if (err) throw err;
-        // console.log(data);
+        console.log(data);
         const getTimeUpdated = data.publishedfiledetails[0].time_updated;
         const getModName = data.publishedfiledetails[0].title;
         const getModID = data.publishedfiledetails[0].publishedfileid;
+        const getModThumbnail = data.publishedfiledetails[0].preview_url;
+        const getModURL = `https://steamcommunity.com/sharedfiles/filedetails/?id=${getModID}`;
 
         let db = new sqlite3.Database("./database.db", (err) => {
           if (err) {
@@ -48,7 +50,7 @@ Steam.ready(async function (err) {
 
         try {
           if (getTimeUpdated > existingMod.last_updated) {
-            console.log("Mod Title: " + getModName, "\n", "Mod ID: " + getModID, "\n", "Last Updated: " + existingMod.last_updated, "\n", "New Updated: " + getTimeUpdated);
+            // console.log("Mod Title: " + getModName, "\n", "Mod ID: " + getModID, "\n", "Last Updated: " + existingMod.last_updated, "\n", "New Updated: " + getTimeUpdated);
             await db.run(`UPDATE mods SET last_updated = ? WHERE mod_id = ?`, [getTimeUpdated, getModID]);
 
             const existingModRecheck = await new Promise((resolve, reject) => {
@@ -60,7 +62,17 @@ Steam.ready(async function (err) {
               });
             });
 
-            console.log(`**MOD UPDATE DETECTED**\n${getModName} has been updated\n New Updated: ${existingModRecheck.last_updated}\n `);
+            const lastUpdateTimestamp = existingMod.last_updated;
+            const lastUpdateDate = new Date(lastUpdateTimestamp * 1000);
+            const lastUpdatedOptions = { month: "long", day: "numeric", year: "numeric", hour: "numeric", minute: "numeric", second: "numeric" };
+            const lastUpdateFormattedDate = lastUpdateDate.toLocaleDateString("en-GB", lastUpdatedOptions);
+
+            const newUpdatedTimestamp = existingModRecheck.last_updated;
+            const newUpdatedDate = new Date(newUpdatedTimestamp * 1000);
+            const newUpdatedOptions = { month: "long", day: "numeric", year: "numeric", hour: "numeric", minute: "numeric", second: "numeric" };
+            const newUpdateFormattedDate = newUpdatedDate.toLocaleDateString("en-GB", newUpdatedOptions);
+
+            console.log(`**MOD UPDATE DETECTED**\n${getModName} has been updated\nMod ID: ${getModID}\nLast Updated Date: ${lastUpdateFormattedDate}\nNew Updated Date: ${newUpdateFormattedDate}\nThumbnail: ${getModThumbnail}\nMod URL: ${getModURL} `);
             await db.close;
           } else {
             console.log("no updates");
@@ -72,5 +84,5 @@ Steam.ready(async function (err) {
       });
     });
   }
-  setInterval(modUpdateCheck, 10000);
+  setInterval(modUpdateCheck, 2000);
 });
